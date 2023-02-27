cf06c44e8ce606b5bf8a90cf3df74f9de72ad32b
Will Kupiec <wkupiec@mit.edu>
2023-02-27 12:28:52 -0500

    Initial Commit

---
+++ Propeller/selection_tools.py
@@ -0,0 +1,14 @@
+# Will Kupiec
+# 2/27/2023
+def beta2pitch(beta_max):
+    '''
+    Estimates the screw pitch angle from the maximum beta angle of a propeller
+    :param beta_max: Maxiumum angle of the propeller blade wrt a flat plane attached to the mount
+    :return: Estimated Pitch, measured in inches
+    '''
+
+
+
+
+if __name__ == "__main__":
+    pass
